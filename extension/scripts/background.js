e19d5097917e4b719bd9fd0c069ba415016eb9d8
Martin Meredith <martin.meredith@hystreet.co.uk>
2018-08-09 11:34:05 +0100

    Add notifications!

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -7,27 +7,37 @@ const statusImages = {
   major: 'images/status-icon-red.png'
 };
 
-const checkStatus = async () => {
-  return fetch('https://status.github.com/api/last-message.json')
-    .then(response => {
-      return response.json();
-    })
+const checkStatus = async (initial = false) =>
+  fetch('https://status.github.com/api/last-message.json')
+    .then(response => response.json())
     .then(data => {
       const status = data.status;
       const statusText = data.body.trim();
+      let statusChanged = false;
 
       if (status !== currentStatus) {
         chrome.browserAction.setIcon({ path: statusImages[status] });
 
         currentStatus = status;
+        statusChanged = true;
       }
 
       if (statusText !== currentStatusText) {
         chrome.browserAction.setTitle({ title: statusText });
+
         currentStatusText = statusText;
+        statusChanged = true;
+      }
+
+      if (statusChanged && !initial) {
+        chrome.notifications.create('githubStatus', {
+          type: 'basic',
+          iconUrl: statusImages[currentStatus],
+          message: currentStatusText,
+          title: 'Github status update'
+        });
       }
     });
-};
 
 // Manage Alarm
 
@@ -47,4 +57,4 @@ chrome.browserAction.onClicked.addListener(() => {
 
 // Initial Check
 
-checkStatus();
+checkStatus(true);
