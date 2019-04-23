3ac2e574cdbff0e72890c35cee359c43101767db
Martin Meredith <martin.meredith@magic42.co.uk>
2019-04-23 10:46:19 +0100

    Update for new API

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,50 +1,76 @@
-let currentStatus = '';
-let currentStatusText = '';
+const ALARM_NAME = 'checkGithubStatus';
+const ALARM_MINUTES = 1;
+const STATUS_URL = 'https://www.githubstatus.com/api/v2/status.json';
 
 const statusImages = {
-  good: 'images/status-icon-green.png',
-  minor: 'images/status-icon-orange.png',
-  major: 'images/status-icon-red.png'
+  none: {
+    '32': 'images/status-icon-black@1.png',
+    '64': 'images/status-icon-black@2.png',
+    '128': 'images/status-icon-black@4.png'
+  },
+  minor: {
+    '32': 'images/status-icon-yellow@1.png',
+    '64': 'images/status-icon-yellow@2.png',
+    '128': 'images/status-icon-yellow@4.png'
+  },
+  major: {
+    '32': 'images/status-icon-orange@1.png',
+    '64': 'images/status-icon-orange@2.png',
+    '128': 'images/status-icon-orange@4.png'
+  },
+  critical: {
+    '32': 'images/status-icon-red@1.png',
+    '64': 'images/status-icon-red@2.png',
+    '128': 'images/status-icon-red@4.png'
+  }
 };
 
-const checkStatus = async (initial = false) =>
-  fetch('https://status.github.com/api/last-message.json')
-    .then(response => response.json())
-    .then(data => {
-      const status = data.status;
-      const statusText = data.body.trim();
-      let statusChanged = false;
+let currentIndicator = '';
+let currentDescription = '';
 
-      if (status !== currentStatus) {
-        chrome.browserAction.setIcon({ path: statusImages[status] });
+const fetchStatus = () =>
+  fetchStatus(STATUS_URL).then(response => response.json());
 
-        currentStatus = status;
-        statusChanged = true;
-      }
+const handleStatus = disableNotification => ({
+  status: { description, indicator }
+}) => {
+  if (statusChanged(indicator, description)) {
+    updateStatus(indicator, description, disableNotification);
+  }
+};
 
-      if (statusText !== currentStatusText) {
-        chrome.browserAction.setTitle({ title: statusText });
+const statusChanged = (indicator, description) =>
+  indicator !== currentIndicator || description !== currentDescription;
 
-        currentStatusText = statusText;
-        statusChanged = true;
-      }
+const notifyUser = () => {
+  chrome.notifications.create('githubStatus', {
+    type: 'basic',
+    iconUrl: statusImages[currentIndicator]['128'],
+    message: currentDescription,
+    title: 'Github status update'
+  });
+};
 
-      if (statusChanged && !initial) {
-        chrome.notifications.create('githubStatus', {
-          type: 'basic',
-          iconUrl: statusImages[currentStatus],
-          message: currentStatusText,
-          title: 'Github status update'
-        });
-      }
-    });
+const updateStatus = (indicator, description, disableNotification) => {
+  chrome.browserAction.setIcon({ path: statusImages[indicator] });
+  chrome.browserAction.setTitle({ title: description });
+
+  currentIndicator = indicator;
+  currentDescription = description;
+
+  if (!disableNotification) {
+    notifyUser();
+  }
+};
+
+const checkStatus = (disableNotification = false) =>
+  fetchStatus().then(handleStatus(disableNotification));
 
 // Manage Alarm
-
-chrome.alarms.create('checkGithubStatus', { periodInMinutes: 1 });
+chrome.alarms.create(ALARM_NAME, { periodInMinutes: ALARM_MINUTES });
 
 chrome.alarms.onAlarm.addListener(alarm => {
-  if (alarm.name === 'checkGithubStatus') {
+  if (alarm.name === ALARM_NAME) {
     checkStatus();
   }
 });
